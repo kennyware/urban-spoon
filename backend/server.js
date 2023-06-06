94284ca0084cc7bb593a7d5758e265e2e33cae8a
Kenny Ware <kennyware34@gmail.com>
2023-06-06 12:17:40 -0500

    removed package-lock file

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const { connectDB } = require("./config/db");
+const path = require("path");
 
 const port = process.env.PORT || 5000;
 
@@ -21,6 +22,15 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 // Error handling middleware
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
