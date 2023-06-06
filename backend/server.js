36fabf19607a2e48d899ffa426e869682c8c5b01
Kenny Ware <kennyware34@gmail.com>
2023-06-06 12:44:27 -0500

    edited server file

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
